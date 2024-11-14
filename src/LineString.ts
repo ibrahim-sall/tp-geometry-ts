d905548a85385cfd9b1572531fa1ff0ac7c5941d
ibrahim-sall <ibhou2002@gmail.com>
2024-11-14 11:35:30 +0100

    Implémentation de Geometry et LineString ainsi que de leurs tests unitaires

---
+++ src/LineString.ts
@@ -0,0 +1,25 @@
+
+import Geometry from "./Geometry";
+import Point from "./Point";
+
+
+export default class LineString implements Geometry {
+    private points?: Array<Point>;
+
+    constructor ();
+    constructor(points:Array<Point>);
+
+   constructor(points?:Array<Point>) {
+        this.points = points ?? [];
+    }
+
+    getNumPoints(): number {
+        return this.points ? this.points.length : 0;
+    }
+    getPointN(n:number) : Point {
+        return this.points ? this.points[n] : null;
+        }
+    getType() :string {
+        return "LineString";
+    }
+}
--- src/Point.ts
+++ src/Point.ts
@@ -1,22 +1,26 @@
 import Coordinate from "./Coordinate";
+import Geometry from "./Geometry";
 
-export default class Point {
+export default class Point implements Geometry {
   private coordinate?: Coordinate;
 
   constructor(coordinate?: Coordinate) {
-    this.coordinate = coordinate ;
+    this.coordinate = coordinate;
   }
 
   getCoordinate(): Coordinate {
     return this.coordinate;
   }
+  getType(): string {
+    return "Point";
+  }
 
   x(): number {
-    return this.coordinate ? this.coordinate[0] : Number.NaN ;
+    return this.coordinate ? this.coordinate[0] : Number.NaN;
   }
 
   y(): number {
-    return this.coordinate ? this.coordinate[1] : Number.NaN ;
+    return this.coordinate ? this.coordinate[1] : Number.NaN;
   }
 
 }
